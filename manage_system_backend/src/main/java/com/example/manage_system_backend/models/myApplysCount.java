c101d48e6811c22210ee88bc9e1d4c9a4be41eec
linshumin <2775147476@qq.com>
2023-05-17 20:57:12 +0800

    "主页加图表"

---
+++ manage_system_backend/src/main/java/com/example/manage_system_backend/models/myApplysCount.java
@@ -0,0 +1,15 @@
+package com.example.manage_system_backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class myApplystatusCount {
+    private String name;
+    private Long value;
+}
